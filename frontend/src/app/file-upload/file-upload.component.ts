59e9e877ccaaeb0289881e195ee91c8eea4c19a5
Qi Yu <qiyu@lap942.sprachwiss.uni-konstanz.de>
2023-11-03 12:34:09 +0100

    added sort function for file upload component

--- frontend/src/app/file-upload/file-upload.component.ts
+++ frontend/src/app/file-upload/file-upload.component.ts
@@ -2,9 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 import { API_URL } from '../env';
 import { IFile } from './file';
-import { MessageService, Message } from 'primeng/api';
+import { MessageService } from 'primeng/api';
 import { FileService } from './file.service';
-import { ButtonModule } from 'primeng/button';
+
 
 @Component({
   selector: 'app-file-upload',
@@ -57,20 +57,34 @@ export class FileUploadComponent implements OnInit {
     });
   }
 
-  onDeleteFiles(): void{
-    for(let i = 0; i < this.filesSelectedForAnalyses.length; i++) {
-      this.fileService.deleteFile(this.filesSelectedForAnalyses[i].filename).subscribe({
+  onDeleteFiles(): void {
+    const filesToDelete = [...this.filesSelectedForAnalyses];
+  
+    for (const file of filesToDelete) {
+      this.fileService.deleteFile(file.filename).subscribe({
         next: () => {
-          this.uploadedFiles = this.uploadedFiles.filter((uploadedFile) => uploadedFile.filename !== this.filesSelectedForAnalyses[i].filename);
-          this.filesSelectedForAnalyses.splice(i, 1);
+          const indexInUploadedFiles = this.uploadedFiles.findIndex((uploadedFile) => uploadedFile.filename === file.filename);
+          if (indexInUploadedFiles !== -1) {
+            this.uploadedFiles.splice(indexInUploadedFiles, 1);
+          }
+  
+          const indexInFilesSelectedForAnalyses = this.filesSelectedForAnalyses.indexOf(file);
+          if (indexInFilesSelectedForAnalyses !== -1) {
+            this.filesSelectedForAnalyses.splice(indexInFilesSelectedForAnalyses, 1);
+          }
+  
           this.onSelectFiles();
         },
         error: () => {
-          this.messageService.add({severity: 'error', summary: 'Error', detail: 'File(s) cannot be deleted successfully!'});
-        }
-      })
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'File(s) cannot be deleted successfully!',
+          });
+        },
+      });
     }
-  }
+  }  
 
   onChangeSelectionStatus(fileToChange: IFile): void {
     this.filesSelectedForAnalyses = this.filesSelectedForAnalyses.filter((file) => file.selectedForAnalyses === true);
