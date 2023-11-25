b17d96c5a97e6470093a7a352f2de81880d39d83
Qi Yu <qiyu@lap942.sprachwiss.uni-konstanz.de>
2023-11-25 23:19:23 +0100

    added function for toggle selecting dimensions

--- frontend/src/app/feature-selection/feature-selection.component.ts
+++ frontend/src/app/feature-selection/feature-selection.component.ts
@@ -12,6 +12,7 @@ export class FeatureSelectionComponent implements OnInit {
   checked: boolean = true;
   selectedFeatures: Array<IFeature> = [];
   allFeatures: Array<IFeature> = [];
+  selectedDimensions: Array<string> = [];
   allDimensions: Array<string> = [];
   errorMsg: any;
 
@@ -23,15 +24,23 @@ export class FeatureSelectionComponent implements OnInit {
     if(this.checked === false) {
       this.featureService.getAllFeatures()
         .subscribe({
-          next: data => this.selectedFeatures = data
+          next: (data) => {
+            this.selectedFeatures = data;
+            this.selectedDimensions = [...new Set(data.map((item) => item.dimension))];
+          }
         })
     } else {
       this.selectedFeatures = [];
+      this.selectedDimensions = [];
     }
   }
 
-  onSelectFeature() {
-    this.checked = false;
+  onSelectFeature(event: any) {
+    this.selectedFeatures.length === 0 ? this.checked  = true : this.checked = false;
+  }
+
+  onSelectDimension(event: any) {
+    this.selectedFeatures = this.allFeatures.filter((feature) => event.checked.includes(feature.dimension));
     this.selectedFeatures.length === 0 ? this.checked  = true : this.checked = false;
   }
 
