d7e3cbc55f0bcdfa6f0a423951dfe461a6dd85bd
Qi Yu <qiyu@lap942.sprachwiss.uni-konstanz.de>
2023-11-25 21:29:25 +0100

    added function for clearing selections

--- frontend/src/app/feature-selection/feature-selection.component.ts
+++ frontend/src/app/feature-selection/feature-selection.component.ts
@@ -13,7 +13,6 @@ export class FeatureSelectionComponent implements OnInit {
   selectedFeatures: Array<IFeature> = [];
   allFeatures: Array<IFeature> = [];
   allDimensions: Array<string> = [];
-  selectAll: boolean = false;
   errorMsg: any;
 
   constructor(private featureService: FeatureService) {
@@ -21,18 +20,19 @@ export class FeatureSelectionComponent implements OnInit {
   }
 
   onToggleSelectAll() {
-    if(this.selectAll === false) {
+    if(this.checked === false) {
       this.featureService.getAllFeatures()
         .subscribe({
           next: data => this.selectedFeatures = data
         })
-
-      this.selectAll = !this.selectAll
-
     } else {
       this.selectedFeatures = [];
-      this.selectAll = !this.selectAll;
     }
+  }
+
+  onSelectFeature() {
+    this.checked = false;
+    this.selectedFeatures.length === 0 ? this.checked  = true : this.checked = false;
   }
 
   ngOnInit(): void {
